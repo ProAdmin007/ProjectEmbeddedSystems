45903ca87295ed7988d89e3ae6e4fa52b4590986
RJHarte <33251295+RJHarte@users.noreply.github.com>
2020-10-26 14:47:27 +0100

    initial commit
    work in progress

---
+++ centrale/main.py
@@ -0,0 +1,149 @@
+import tkinter as tk
+
+
+class Main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self, "Trollluik Interface")
+        self.resizable(0, 0)
+        self.geometry('400x300')
+        self.iconbitmap('d:/Documents/2.1/logo.ico')                #to-do make portable
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (HomeScreen, RoomPage, ScreenPage, AddRoom, AddScreen):         
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("HomeScreen")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    #def welcomeMessage(self):
+
+class HomeScreen(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        lframe = tk.LabelFrame(self, padx=5, pady=5)
+        rframe = tk.LabelFrame(self, padx=5, pady=5)
+
+        lframe.grid(row= 0, column = 0, padx=5, pady=5)
+        rframe.grid(row= 0, column = 1, padx=5, pady=5, rowspan = 10)
+
+        welcome = tk.Label(rframe, wraplength = 150, text = "balkbalssjhdgfsajd gfasjdfg sajdhfgasdkjhf gasdjhf gaskdlfjg askldfgaskldjf hasjkdfh")
+        rooms = tk.Label(lframe, text = "Kamers")
+        add = tk.Button(lframe, text = "  +  ", command = lambda: controller.show_frame("AddRoom"))           #word pop-up ipv framelayer ?
+        remove = tk.Button(lframe, text = "  -  ", command = lambda: controller.show_frame("RemoveRoom"))     #word pop-up ipv framelayer
+        listbox = tk.Listbox(lframe)
+        for values in range(100):                           #placeholder "list"
+            listbox.insert(tk.END, values) 
+        scrollbar = tk.Scrollbar(lframe,)
+
+          
+        listbox.config(yscrollcommand = scrollbar.set)
+        scrollbar.config(command = listbox.yview) 
+
+        welcome.pack()
+        rooms.grid(row = 0, column = 0)
+        listbox.grid(row = 1, column = 0, padx = 5, pady =5)
+        scrollbar.grid(row = 1, column = 1,)
+        add.grid(row = 2, column = 0, padx = 10, pady = 5)
+        remove.grid(row = 2, column = 1, padx = 10, pady = 5)
+
+
+#    def removeRoomWindow(self):
+#        self.newWindow2 = tk.Toplevel(self.master)
+#        self.main = RemoveRoom(self.removeRoomWindow)
+
+
+class RoomPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+    def addScreenWindow(self):
+        self.newWindow3 = tk.Toplevel(self.master)
+        self.app = RemoveScreen(self.addScreenWindow)
+
+    def removeScreenWindow(self):
+        self.newWindow4 = tk.Toplevel(self.master)
+        self.app = RemoveScreen(self.removeScreenWindow)
+
+class ScreenPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+class AddRoom(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        RoomMessage = tk.Label(self, text = "Kamer toevoegen")
+        Name = tk.Label(text = "Naam:")
+        NameEntry = tk.Entry()
+        AddButton = tk.Button()
+        CancelButton = tk.Button()
+    
+        RoomMessage.grid()
+        Name.grid()
+        NameEntry.grid()
+        AddButton.grid()
+        CancelButton.grid()
+
+
+class RemoveRoom(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        RoomMessage = tk.Label(self, text = "Kamer toevoegen")
+        RoomMessage.pack()
+
+
+class AddScreen(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+class RemoveScreen(tk.Frame):
+
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+
+class room:
+
+    def __init__(self, name):
+        self.name = name
+
+class screen:
+    
+    def __init__(self, name, room, device):
+        self.name = name
+        self.room = room
+        self.device = device
+
+
+if __name__ == "__main__":
+    root = Main()
+    root.mainloop()
