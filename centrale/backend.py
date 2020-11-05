1056fd401519e6beb2e17560ab2b76cc683ff5dd
unknown <s.wierenga@st.hanze.nl>
2020-11-05 13:43:02 +0100

    Data class fixed

--- centrale/backend.py
+++ centrale/backend.py
@@ -1,26 +1,42 @@
 import json
 import os.path
 
-class Main():
-    def __init__(self,fileName):
-        self.data = self.openjson(fileName)
+class Data():
+    def __init__(self,fileName,dirName):
+        self.fileName = fileName
+        print(self.fileName)
+        self.dirname = dirName
+        self.data = self.openjson()
 
-    def filecheck(self,fileName):
-        file = os.path.isfile(fileName)
+    def filecheck(self):
+        file = os.path.isfile(self.fileName)
+        print(file)
         if not file:
-            open(fileName,"x")
-            self.writejson(fileName,{"Kamers":[]})
+            open(self.fileName,"x")
+            self.writejson({self.dirname:[]})
             return False
         else:
             return True
         
-    def openjson(self,fileName):
-        self.filecheck(fileName)
-        with open(fileName) as json_file:
+    def openjson(self):
+        self.filecheck()
+        with open(self.fileName) as json_file:
             return json.load(json_file)
     
-    def writejson(self,fileName,jsonData):
-        with open(fileName, 'w') as outfile:
+    def writejson(self,jsonData):
+        with open(self.fileName, 'w') as outfile:
             json.dump(jsonData, outfile)
     
-Main("test.json")
+    def getjson():
+        return self.data
+    
+    def addjson(item,data):
+        for i in self.data[self.dirname]:
+            if i[item] == data[item]:
+                i = data
+                return True
+        return false
+
+
+
+Data("config.json","Kamers")
