7aaf85f675bd4b5db4f0075df94c1f685fccdee5
Ruben Weijer <ruben.weijer@cloudsters.nl>
2020-11-05 16:05:42 +0100

    Data read form Arduion - added if statment

--- centrale/Berekening.py
+++ centrale/Berekening.py
@@ -1,22 +1,56 @@
 import serial
+import time
+
 ser = serial.Serial('COM3', 9600,timeout=10)
-data = ser.read(2)
+data = ser.read(1)
 #data.split("x")
 data_dict = {}
-print("Datatype is: ",type(data))
+#print("Datatype is: ",type(data))
+
+#split_data = data[1:2]
 
 dataInt = int.from_bytes(data, byteorder='big')
+dataString = data.hex()
+print("Hex data bit 1: ",dataString)
 
-split_data = data[1:2]
+""" teller=1
+while teller<=1:
+    data_dict.update({data[0:1]:data[1:2]})
+    #data.update({'c':3,'d':4})  # Updates 'c' and adds 'd'
+    time.sleep(5)
+    print(teller)
+    teller+=1
+ """
+#dataInt2 = int.from_bytes(split_data, byteorder='big')
 
-dataInt2 = int.from_bytes(split_data, byteorder='big')
-
-print("dit is data: ")
+print("dit is Bit1: ")
 print(data)
+print("dit is Bit2: ")
+data2 = ser.read(1)
+dataInt2 = int.from_bytes(data2, byteorder='big')
+print(dataInt2)
+print("dit is Bit3: ")
+data3 = ser.read(1)
+print(data3)
+print("dit is Bit4: ")
+data4 = ser.read(1)
+print(data4)
 print("dit is dataInt: ")
 print(dataInt)
 print("dit is split_data: ")
-print(split_data)
+#print(split_data)
 print("dit is dataInt2: ")
-print(dataInt2)
-#print(int(data, 0))
+#print(dataInt2)
+print("dit is data_dict: ")
+for item in data_dict.items():
+    print(item)
+
+
+#concept ifje
+if data == bytes(b'L'):
+    print("Data is Lichtdata")
+    print(ser.read(2))
+
+elif data3 == bytes(b'T'):
+    print("Data is Tempdata")
+    print(ser.read(4))
