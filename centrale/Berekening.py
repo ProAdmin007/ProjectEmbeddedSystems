4a4083bf3187122a93c0b5e51f9a2fc707f98cc3
ruben w <ruben.weijer@cloudsters.nl>
2020-11-06 15:23:26 +0100

    Comment added

--- centrale/Berekening.py
+++ centrale/Berekening.py
@@ -1,28 +1,18 @@
 import serial
 import time
 
-ser = serial.Serial('COM1', 9600,timeout=10)
-data = ser.read(1)
-#data.split("x")
-data_dict = {}
-#print("Datatype is: ",type(data))
-#print(ser.read(24))
-#split_data = data[1:2]
+ser = serial.Serial('COM1', 9600,timeout=10) #open connectie op poort COM1
+data = ser.read(1) # leest het eerste bit
 
-dataInt = int.from_bytes(data, byteorder='big')
+data_dict = {} # Placeholder dict
+
+dataInt = int.from_bytes(data, byteorder='big') # Bytes naar int omzetten
+
+# Dat naar hex functie
 dataString = data.hex()
 print("Hex data bit 1: ",dataString)
 
-""" teller=1
-while teller<=1:
-    data_dict.update({data[0:1]:data[1:2]})
-    #data.update({'c':3,'d':4})  # Updates 'c' and adds 'd'
-    time.sleep(5)
-    print(teller)
-    teller+=1
- """
-#dataInt2 = int.from_bytes(split_data, byteorder='big')
-
+# Test print statmens
 print("dit is Bit1: ")
 print(data)
 print("dit is Bit2: ")
@@ -37,18 +27,10 @@ data4 = ser.read(1)
 print(data4)
 print("dit is dataInt: ")
 print(dataInt)
-#print("dit is split_data: ")
-#print(split_data)
-#print("dit is dataInt2: ")
-#print(dataInt2)
-print("dit is data_dict: ")
-for item in data_dict.items():
-    print(item)
 
 i = 100
-while i == 100:
-    #concept ifje
-    if data == bytes(b'L'):
+while i == 100: # Infenet loop
+    if data == bytes(b'L'): # If die checkt of het lichtdata is
         print("Data is Lichtdata")
         bit2 = ser.read(2)
         print(bit2)
@@ -56,9 +38,9 @@ while i == 100:
         print(lichtData)
         print(int.from_bytes(lichtData, byteorder='big'))
 
-    if data3 == bytes(b'T'):
+    if data3 == bytes(b'T'): # If die checkt of het tempratuurdata is
         print("Data is Tempdata")
-        bit4 = ser.read(4)
+        bit4 = ser.read(2)
         print(bit4)
         tempData = bit4[1:3]
         print(tempData)
