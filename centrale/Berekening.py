96c32ffe706a7407ea86b73bd24f3fa8c92cb82a
ruben w <ruben.weijer@cloudsters.nl>
2020-11-06 15:11:10 +0100

    If statemant to check incoming data

--- centrale/Berekening.py
+++ centrale/Berekening.py
@@ -1,12 +1,12 @@
 import serial
 import time
 
-ser = serial.Serial('COM3', 9600,timeout=10)
+ser = serial.Serial('COM1', 9600,timeout=10)
 data = ser.read(1)
 #data.split("x")
 data_dict = {}
 #print("Datatype is: ",type(data))
-
+#print(ser.read(24))
 #split_data = data[1:2]
 
 dataInt = int.from_bytes(data, byteorder='big')
@@ -37,20 +37,29 @@ data4 = ser.read(1)
 print(data4)
 print("dit is dataInt: ")
 print(dataInt)
-print("dit is split_data: ")
+#print("dit is split_data: ")
 #print(split_data)
-print("dit is dataInt2: ")
+#print("dit is dataInt2: ")
 #print(dataInt2)
 print("dit is data_dict: ")
 for item in data_dict.items():
     print(item)
 
+i = 100
+while i == 100:
+    #concept ifje
+    if data == bytes(b'L'):
+        print("Data is Lichtdata")
+        bit2 = ser.read(2)
+        print(bit2)
+        lichtData = bit2[1:4]
+        print(lichtData)
+        print(int.from_bytes(lichtData, byteorder='big'))
 
-#concept ifje
-if data == bytes(b'L'):
-    print("Data is Lichtdata")
-    print(ser.read(2))
-
-elif data3 == bytes(b'T'):
-    print("Data is Tempdata")
-    print(ser.read(4))
+    if data3 == bytes(b'T'):
+        print("Data is Tempdata")
+        bit4 = ser.read(4)
+        print(bit4)
+        tempData = bit4[1:3]
+        print(tempData)
+        print(int.from_bytes(tempData, byteorder='big'))
