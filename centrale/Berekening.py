dc8c0f80e04704c948eaa63ecb2a7aa58f28f129
Ruben Weijer <ruben.weijer@cloudsters.nl>
2020-11-07 15:56:59 +0100

    Berekeningen naar 1 bestand

--- centrale/Berekening.py
+++ centrale/Berekening.py
@@ -1,52 +1,29 @@
 import serial
-import time
 
-ser = serial.Serial('COM1', 9600,timeout=10) #open connectie op poort COM1
-data = ser.read(1) # leest het eerste bit
+# open connectie op poort COM1
+ser = serial.Serial('COM3', 9600,timeout=10)
 
-data_dict = {} # Placeholder dict
-
-dataInt = int.from_bytes(data, byteorder='big') # Bytes naar int omzetten
-
-# Dat naar hex functie
-dataString = data.hex()
-print("Hex data bit 1: ",dataString)
-
-# Test print statmens
-print("dit is Bit1: ")
-print(data)
-print("dit is Bit2: ")
-data2 = ser.read(1)
-dataInt2 = int.from_bytes(data2, byteorder='big')
-print(dataInt2)
-print("dit is Bit3: ")
-data3 = ser.read(1)
-print(data3)
-print("dit is Bit4: ")
-data4 = ser.read(1)
-print(data4)
-print("dit is dataInt: ")
-print(dataInt)
-
-i = 100
-while i == 100: # Infenet loop
-    if data == bytes(b'L'): # If die checkt of het lichtdata is
-        print("Data is Lichtdata")
-        bit2 = ser.read(2)
-        print(bit2)
-        lichtData = bit2[1:4]
-        print(lichtData)
-        print(int.from_bytes(lichtData, byteorder='big'))
-
-    if data3 == bytes(b'T'): # If die checkt of het tempratuurdata is
-        print("Data is Tempdata")
-        bit4 = ser.read(2)
-        print(bit4)
-        tempData = bit4[1:3]
-        print(tempData)
-        tempDataInt = int.from_bytes(tempData, byteorder='big')
-        print(tempDataInt)
-        # Berekening naar tempC
-        voltageOut = (tempDataInt * 5000) / 1024
-        temperatureC = round((voltageOut / 10) * 6)
-        print("TEMP IS ------- ", temperatureC)
+# infenent loop to read data
+while True:
+    # read the first command
+    # leest het eerste bit
+    # 0x41 => next byte is distance data
+    # 0x4C => next byte is light data
+    # 0x54 => next byte is temp data
+    command = ser.read().hex()
+    if command == '41':
+        dist_data = ser.read().hex()
+        dist_dec = int(dist_data, 16)
+        #print('distance - 0x{}'.format(dist_data))
+        print('Distance in decimaal = ',dist_dec)
+    if command == '4c':
+        light_data = ser.read().hex()
+        light_dec = int(light_data, 16)
+        #print('light - 0x{}'.format(light_data))
+        print('Light in decimaal = ',light_dec)
+    if command == '54':
+        temp_data = ser.read().hex()
+        voltageOut = (int(temp_data, 16) * 5000) / 1024
+        temperatureC = round((voltageOut / 10) * 4)
+        #print('temp - 0x{}'.format(temp_data))
+        print('Temp in C = ',temperatureC)
