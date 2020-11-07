49b8d7ce1926f746a94abc8f4ef1794bef7f5c92
Ruben Weijer <ruben.weijer@cloudsters.nl>
2020-11-07 15:43:58 +0100

    Littel comment fixt

--- centrale/berekeningV2.py
+++ centrale/berekeningV2.py
@@ -1,8 +1,9 @@
 import serial
 
-ser = serial.Serial('COM3', 9600,timeout=10) #open connectie op poort COM1
+# open connectie op poort COM1
+ser = serial.Serial('COM3', 9600,timeout=10)
 
-
+# infenent loop to read data
 while True:
     # read the first command
     # leest het eerste bit
@@ -14,15 +15,15 @@ while True:
         dist_data = ser.read().hex()
         dist_dec = int(dist_data, 16)
         #print('distance - 0x{}'.format(dist_data))
-        print('distance in decimaal - ',dist_dec)
+        print('Distance in decimaal = ',dist_dec)
     if command == '4c':
         light_data = ser.read().hex()
         light_dec = int(light_data, 16)
         #print('light - 0x{}'.format(light_data))
-        print('light in decimaal = ',light_dec)
+        print('Light in decimaal = ',light_dec)
     if command == '54':
         temp_data = ser.read().hex()
         voltageOut = (int(temp_data, 16) * 5000) / 1024
         temperatureC = round((voltageOut / 10) * 4)
         #print('temp - 0x{}'.format(temp_data))
-        print('temp in C - ',temperatureC)
+        print('Temp in C = ',temperatureC)
