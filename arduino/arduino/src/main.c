fd004b1569fa652aff6e496d1ddfddcb6bbc661e
brammosso <derpedederpderp@gmail.com>
2020-11-08 14:58:14 +0100

    Fixed bugs with average values
    Added a delay before data will be send of 5 seconds.
    Removed a debug value which made the distance sensor fire all the time.

--- arduino/arduino/src/main.c
+++ arduino/arduino/src/main.c
@@ -28,7 +28,7 @@ int main (void){
 		if(temp_ready){
 			send_temp();
 		}
-		if(1){
+		if(distance_ready){
 			send_distance();
 		}
 		check_command();
@@ -60,13 +60,13 @@ void init(){
 }
 
 void scheduler_tasks(){
-	SCH_Add_Task(set_light_bool, 0, 10);		// average light sent every 30 seconds
-	SCH_Add_Task(set_temp_bool, 0, 15);			// average temp sent every 40 seconds
-	SCH_Add_Task(set_distance_bool, 0, 20);		// average distance sent every 60 seconds
-
 	SCH_Add_Task(avg_light, 0, 1);				// average light measured in second increments
 	SCH_Add_Task(avg_temp, 0, 1);				// average temp measured in second increments
 	SCH_Add_Task(avg_dist, 0, 1);				// average distance measured in second increments
+	
+	SCH_Add_Task(set_light_bool, 5, 10);		// average light sent every 30 seconds
+	SCH_Add_Task(set_temp_bool, 5, 15);			// average temp sent every 40 seconds
+	SCH_Add_Task(set_distance_bool, 5, 30);		// average distance sent every 60 seconds
 }
 
 void send_light(){
