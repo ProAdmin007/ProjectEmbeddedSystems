622045ba81376ae9c0e30253acf33feda4b19876
Ruben Weijer <ruben.weijer@cloudsters.nl>
2020-11-09 11:12:14 +0100

    Berekeningen toegevoegd aan backend.py

--- centrale/backend.py
+++ centrale/backend.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import serial
 
 class Data():
     def __init__(self, fileName, dirName):
@@ -28,6 +29,39 @@ class Data():
     def getjson(self):
         self.data = self.openjson()
         return self.data
+
+    def connector(self, comport):
+        # open connectie op poort COMport
+        self.ser = serial.Serial(comport, 9600,timeout=10)
+
+    def bereken(self):
+        # infenent loop to read data
+        while True:
+            # read the first command
+            # leest het eerste bit
+            # 0x41 => next byte is distance data
+            # 0x4C => next byte is light data
+            # 0x54 => next byte is temp data
+            command = self.ser.read().hex()
+            if command == '41':
+                dist_data = self.ser.read().hex()
+                dist_dec = int(dist_data, 16)
+                #print('distance - 0x{}'.format(dist_data))
+                print('Distance in decimaal = ',dist_dec)
+                return dist_dec
+            if command == '4c':
+                light_data = self.ser.read().hex()
+                light_dec = int(light_data, 16)
+                #print('light - 0x{}'.format(light_data))
+                print('Light in decimaal = ',light_dec)
+                return light_dec
+            if command == '54':
+                temp_data = self.ser.read().hex()
+                voltageOut = (int(temp_data, 16) * 5000) / 1024
+                temperatureC = round((voltageOut / 10) * 4)
+                #print('temp - 0x{}'.format(temp_data))
+                print('Temp in C = ',temperatureC)
+                return temperatureC
 
 # test =Data ("config.json","Kamers")
 # testdata = test.getjson()
